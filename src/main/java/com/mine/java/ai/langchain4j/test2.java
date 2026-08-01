d5b7005b13d704716491db4575ef00579740df13
relike-en <z.l22.5.2202@gmail.com>
2026-08-01 13:19:59 +0800

    resolve branch1 and branch2 conflict

--- src/main/java/com/mine/java/ai/langchain4j/test2.java
+++ src/main/java/com/mine/java/ai/langchain4j/test2.java
@@ -2,6 +2,8 @@ package com.mine.java.ai.langchain4j;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("day???");
+
+        System.out.println("like-em????");
+
     }
 }
