897d0fd0d537e8b4969bb78b146dac3d26ae1105
relike-en <z.l22.5.2202@gmail.com>
2026-08-01 13:16:41 +0800

    test2 change to day

--- src/main/java/com/mine/java/ai/langchain4j/test2.java
+++ src/main/java/com/mine/java/ai/langchain4j/test2.java
@@ -2,6 +2,6 @@ package com.mine.java.ai.langchain4j;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("Hello???");
+        System.out.println("day???");
     }
 }
